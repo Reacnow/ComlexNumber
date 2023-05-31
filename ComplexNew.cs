3494414cc9c7f8a40a0012268bc474ca3ac4483a
admin <admin@44-10>
2023-05-31 10:09:23 +0300

    Добавьте файлы проекта.

---
+++ ComplexNew.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComlexNumber
+{
+    internal class ComplexNew
+    {
+        private double r, i;
+        private static uint Counter;
+
+        public ComplexNew()
+        {
+            r = 0;
+            i = 0;
+            Counter++;
+        }
+        public ComplexNew(double Re, double Im)
+        {
+            r = Re; i = Im;
+            Counter++;
+        }
+        public ComplexNew(ComplexNew c)
+        {
+            r = c.r; i = c.i; Counter++;
+        }
+        ~ComplexNew()
+        {
+            Counter--;
+        }
+        public double Abs()
+        {
+            return Math.Sqrt(r * r + i * i);
+        }
+        public static ComplexNew operator +(ComplexNew c1, ComplexNew c2)
+        {
+            return new ComplexNew(c1.r + c2.r, c1.i + c2.i);
+        }
+        public void PrintComp()
+        {
+            Console.WriteLine(" Комплексное число {0:0.0} + {1:0.0}i", r, i);
+        }
+        public static uint HowMany()
+        {
+            return Counter;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+using ComlexNumber;
+Console.WriteLine(" Количество комплексных чисел {0}", ComplexNew.HowMany());
+ComplexNew[] a;
+ComplexNew d;
+a = new ComplexNew[3];
+a[0] = new ComplexNew(-1 ,1);
+a[1] = new ComplexNew(2, 5);
+a[2] = new ComplexNew(3, 0);
+Console.WriteLine(" Количество комплексных чисел {0} ", ComplexNew.HowMany());
+d = new ComplexNew();
+for (int i = 0; i < a.Length; i++)
+{
+    a[i].PrintComp();
+    d = d + a[i];
+}
+Console.Write(" Сумма равна "); d.PrintComp();
+Console.WriteLine(" Модуль суммы равен {0:0.00}", d.Abs());
+Console.WriteLine(" Количество комплексных чисел {0}", ComplexNew.HowMany());
+Console.ReadKey();
